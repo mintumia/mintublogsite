477b18405030f57a657f62845bfbb0a21cb08e85
PROPERTY\214843 <mintumia8@gmail.com>
2023-05-16 19:34:10 +0600

    Authenticates Database Added

---
+++ database/migrations/2023_05_16_130918_create_authenticates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('authenticates', function (Blueprint $table) {
+            $table->id();//id	name	gender	email	password	url	is_active
+            $table->string('name',120);
+            $table->enum('gender',["M","F","O"]);
+            $table->string('email',150);
+            $table->string('password');
+            $table->string('url',32);
+            $table->boolean('is_active')->default(1);
+            $table->date('last_login_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('authenticates');
+    }
+};
