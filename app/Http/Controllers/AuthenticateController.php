95f6019db17575af1fa7c9f538e3a0f7b72c0e3c
PROPERTY\214843 <mintumia8@gmail.com>
2023-05-17 11:11:22 +0600

    Authenticate Resource Controller Added

---
+++ app/Http/Controllers/AuthenticateController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthenticateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    private $method_no = 0;
+    public function index()
+    {
+        echo "Hello From ".get_class_methods($this)[$this->method_no = 0];
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        echo "Hello From ".get_class_methods($this)[$this->method_no = 1];
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        echo "Hello From ".get_class_methods($this)[$this->method_no = 2];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        echo "Hello From ".get_class_methods($this)[$this->method_no = 3];
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        echo "Hello From ".get_class_methods($this)[$this->method_no = 4];
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        echo "Hello From ".get_class_methods($this)[$this->method_no = 5];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        echo "Hello From ".get_class_methods($this)[$this->method_no = 6];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticateController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Route::get('user',[AuthenticateController::class,'index']);
+Route::resource('user',AuthenticateController::class);
