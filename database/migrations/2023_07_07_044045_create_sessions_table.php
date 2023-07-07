2e2e4f8a8d72e15eb9be4294090a06aa2195c128
DESKTOP-OPF9AJC\Android_PC <mintumia8@gmail.com>
2023-07-07 11:07:31 +0600

    jet stream added

--- database/migrations/2023_07_07_044045_create_sessions_table.php
+++ database/migrations/2023_07_07_044045_create_sessions_table.php
@@ -17,8 +17,7 @@ return new class extends Migration
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
+            $table->integer('last_activity')->index});
     }
 
     /**
